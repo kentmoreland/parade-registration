9b6a3423f2c2925a2034810a9f9c65985725fdd1
Kent <kentmoreland@gmail.com>
2016-08-29 18:21:00 -0500

    added angular controllers

---
+++ client/js/controllers/entryController.js
@@ -0,0 +1,20 @@
+angular.module('paradeEntry', ['ngResource']).controller('entryController', ['$scope', '$resource', function($scope, $resource){
+  var Entry = $resource('/api/entries');
+
+  Entry.query(function(results){
+    $scope.entries = results;
+  });
+
+  $scope.entries = [];
+
+  $scope.submitEntry = function(){
+    var entry = new Entry();
+    entry.name = $scope.entryName;
+    entry.$save(function(result){
+      $scope.entries.push(result);
+      $scope.entryName = '';
+    })
+  }
+
+}]);
+
