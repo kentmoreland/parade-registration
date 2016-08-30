27ba7aa8085d40fbf90be09ec2a2251c284ab990
Kent <kentmoreland@gmail.com>
2016-08-30 04:36:44 -0500

    added more categories to the schema

--- server/models/entries.js
+++ server/models/entries.js
@@ -3,7 +3,16 @@ mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EntrySchema = new Schema({
-  name: String
+  name: String,
+  email: String,
+  organization: String,
+  category: String,
+  description: String,
+  contact: String,
+  number: String,
+  date: {type: Date, default: Date.now}
+
+
 });
 
 module.exports = mongoose.model('Entry', EntrySchema);
